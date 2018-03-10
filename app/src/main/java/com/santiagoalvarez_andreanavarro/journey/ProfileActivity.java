97c7b93d432f49b1836854971a8c83afc2eb7928
twister10 <alvarez.santiago93@gmail.com>
2018-03-09 19:42:14 -0500

    Initial commit

---
+++ app/src/main/java/com/santiagoalvarez_andreanavarro/journey/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.santiagoalvarez_andreanavarro.journey;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Santiago on 9/03/2018.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ app/src/main/java/com/santiagoalvarez_andreanavarro/journey/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.santiagoalvarez_andreanavarro.journey;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Santiago on 9/03/2018.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_register);
+    }
+
+    public void signup(View view) {
+    }
+}
